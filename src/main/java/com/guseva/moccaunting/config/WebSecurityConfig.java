91d1cc65eba5dd7dfadd9b4f787c0669f5177f27
lenivoe <levishok@gmail.com>
2020-07-24 03:34:40 +0700

    authentication dummy

--- src/main/java/com/guseva/moccaunting/config/WebSecurityConfig.java
+++ src/main/java/com/guseva/moccaunting/config/WebSecurityConfig.java
@@ -1,25 +1,13 @@
 package com.guseva.moccaunting.config;
 
-import com.guseva.moccaunting.domain.User;
-import com.guseva.moccaunting.repo.UserDetailsRepo;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 //@EnableWebSecurity
 //@PropertySource("classpath:application.properties")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private static List<String> clients = Arrays.asList("google");
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -27,34 +15,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest().authenticated()
             .and()
             .oauth2Login();
-    }
-
-    @Bean
-    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
-
-        return map -> {
-            return new User();
-        };
-
-//        return map -> {
-//            String id = (String) map.get("sub");
-//
-//            User user = userDetailsRepo.findById(id).orElseGet(() -> {
-//                User newUser = new User();
-//
-//                newUser.setId(id);
-//                newUser.setName((String) map.get("name"));
-//                newUser.setEmail((String) map.get("email"));
-//                newUser.setGender((String) map.get("gender"));
-//                newUser.setLocale((String) map.get("locale"));
-//                newUser.setUserpic((String) map.get("picture"));
-//
-//                return newUser;
-//            });
-//
-//            user.setLastVisit(LocalDateTime.now());
-//
-//            return userDetailsRepo.save(user);
-//        };
     }
 }
